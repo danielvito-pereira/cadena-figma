218ae3f761abfc33f9ce2ec1cbdbd81e12312ead
danielvito-pereira <danielvitor081@gmail.com>
2023-04-24 10:44:46 -0300

    ajustes no script do menu mobile

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-/* Quando clico no .button, .nav TOGGLE 'activo' */
-const button = document.querySelector('button');/*me permite selecionar os dois elementos  */
-const nav    = document.querySelector('navigation');
+/* Quando clico no .button, .nav TOGGLE 'activo' *//*
+const button = document.querySelector('button');me permite selecionar os dois elementos  
+const nav = document.querySelector('navigation');
 
 button.addEventListener('click',()=>{
-  nav.classList.toggle('ativo')
+  nav.classList.toggle('ativo');
 })
+*/
+
+// Adiciona ou remove a classe "ativo" no elemento de navegação ao clicar no botão.
+const button = document.querySelector('button');
+const nav = document.querySelector('nav');
+
+if (button && nav) {
+  button.addEventListener('click', () => {
+    nav.classList.toggle('ativo');
+  });}else {
+  console.log("não funcionou")
+}
